2d3345d4e3e22676867858dbfe664a68c13e9e62
Jeremy Davis <jeremyrdavis@mac.com>
2020-01-10 11:17:51 +0100

    working

--- quarkus-cafe-web/src/main/java/com/redhat/quarkus/cafe/web/infrastructure/DashboardResource.java
+++ quarkus-cafe-web/src/main/java/com/redhat/quarkus/cafe/web/infrastructure/DashboardResource.java
@@ -1,8 +1,12 @@
 package com.redhat.quarkus.cafe.web.infrastructure;
 
 import com.redhat.quarkus.cafe.web.domain.Update;
+import io.reactivex.Flowable;
 import io.smallrye.reactive.messaging.annotations.Channel;
 import io.smallrye.reactive.messaging.annotations.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
 import org.jboss.resteasy.annotations.SseElementType;
 import org.reactivestreams.Publisher;
 
@@ -18,27 +22,32 @@ import javax.ws.rs.core.MediaType;
 @Path("/dashboard")
 public class DashboardResource {
 
-    @Inject
-    @Channel("dashboard")
+    @Inject @Channel("dashboard")
     Publisher<String> updates;
 
-    @Inject @Channel("dashboard")
+    @Inject @Channel("next")
+    Publisher<String> updatesInternal;
+
+    @Inject @Channel("post")
     Emitter<String> updateEmitter;
 
     Jsonb jsonb = JsonbBuilder.create();
+
 
     @GET
     @Path("/stream")
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @SseElementType("text/plain")
     public Publisher<String> stream() {
-        return updates;
+        return Flowable.merge(updates, updatesInternal)
+                .doOnSubscribe(s -> System.out.println("subscribing"))
+                .doOnNext(s -> System.out.println("Got " + s));
     }
 
     @POST
     @Path("/update")
     public void updateDashboard(Update update) {
-        System.out.println(jsonb.toJson(update).toString());
+        System.out.println("POST: " + jsonb.toJson(update).toString());
         updateEmitter.send(jsonb.toJson(update).toString());
     }
 
--- quarkus-cafe-web/src/main/java/com/redhat/quarkus/cafe/web/infrastructure/EventConverter.java
+++ quarkus-cafe-web/src/main/java/com/redhat/quarkus/cafe/web/infrastructure/EventConverter.java
@@ -5,18 +5,22 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
 
 @ApplicationScoped
 public class EventConverter {
 
-    Jsonb jsonb = JsonbBuilder.create();
-
     @Incoming("webui")
     @Outgoing("dashboard")
     @Broadcast
-    public String process(String event) {
+    public String convert(String event) {
+        System.out.println("Kafka: " + event);
         return event;
+    }
+
+    @Incoming("post")
+    @Outgoing("next")
+    @Broadcast
+    public String id(String x) {
+        return x;
     }
 }
