0e64ef8110be95780a3e190d29934a2746d45415
Jeremy Davis <jeremyrdavis@mac.com>
2020-01-10 18:08:33 +0100

    initial commit

---
+++ quarkus-cafe-kitchen/src/test/java/com/redhat/quarkus/cafe/kitchen/infrastructure/NativeApiResourceIT.java
@@ -0,0 +1,9 @@
+package com.redhat.quarkus.cafe.kitchen.infrastructure;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeApiResourceIT extends ApiResourceTest {
+
+    // Execute the same tests but in native mode.
+}
