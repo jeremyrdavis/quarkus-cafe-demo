374ec1dd9f7add3764fd9b112146c139dc457f6f
Jeremy Davis <jeremyrdavis@mac.com>
2020-01-12 06:50:33 +0100

    updated domain and KafkaResource

---
+++ quarkus-cafe-kitchen/src/main/java/com/redhat/quarkus/cafe/kitchen/domain/OrderEvent.java
@@ -0,0 +1,50 @@
+package com.redhat.quarkus.cafe.kitchen.domain;
+
+import java.util.UUID;
+
+/**
+ * This class represents all orders in the system
+ */
+public class OrderEvent {
+
+    public String itemId;
+    public EventType eventType;
+    public String orderId;
+    public String name;
+    public Item item;
+
+    public OrderEvent(String orderId, String name, Item item, String itemId, EventType eventType) {
+        this.eventType = eventType;
+        this.orderId = orderId;
+        this.name = name;
+        this.item = item;
+    }
+
+    public String getOrderId(){
+        return orderId;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public EventType getEventType(){
+        return eventType;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("OrderEvent[orderId=")
+                .append(orderId)
+                .append(",eventType=")
+                .append(eventType.toString())
+                .append(",item=")
+                .append(item)
+                .append(",name=")
+                .append(name)
+                .append(",itemId=")
+                .append(itemId)
+                .append("]")
+                .toString();
+    }
+}
