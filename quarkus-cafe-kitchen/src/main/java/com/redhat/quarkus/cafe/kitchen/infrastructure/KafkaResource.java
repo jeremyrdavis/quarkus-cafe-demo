a3e9e34c811ab24bf442e6515b1f9f2e6a414612
Jeremy Davis <jeremyrdavis@mac.com>
2020-01-12 18:18:17 -0500

    kitchen now round tripping

--- quarkus-cafe-kitchen/src/main/java/com/redhat/quarkus/cafe/kitchen/infrastructure/KafkaResource.java
+++ quarkus-cafe-kitchen/src/main/java/com/redhat/quarkus/cafe/kitchen/infrastructure/KafkaResource.java
@@ -3,10 +3,14 @@ package com.redhat.quarkus.cafe.kitchen.infrastructure;
 import com.redhat.quarkus.cafe.kitchen.domain.EventType;
 import com.redhat.quarkus.cafe.kitchen.domain.Kitchen;
 import com.redhat.quarkus.cafe.kitchen.domain.OrderEvent;
+import io.vertx.core.Vertx;
+import io.vertx.kafka.client.producer.KafkaProducer;
+import io.vertx.kafka.client.producer.KafkaProducerRecord;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.json.Json;
@@ -15,18 +19,28 @@ import javax.json.JsonReader;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 @ApplicationScoped
 public class KafkaResource {
 
+    private static final String TOPIC = "orders";
+
     Logger logger = Logger.getLogger(KafkaResource.class);
 
     @Inject
     Kitchen kitchen;
 
-    Jsonb jsonb = JsonbBuilder.create();
+    @Inject
+    private Vertx vertx;
+
+    private KafkaProducer<String, String> producer;
+
+    private Jsonb jsonb = JsonbBuilder.create();
 
     @Incoming("orderin")
     public void orderIn(String message) {
@@ -58,6 +72,30 @@ public class KafkaResource {
 
     private void updateKafka(final OrderEvent orderEvent) {
         System.out.println("\nNow update Kafka!");
+        logger.debug("\nSending:" + orderEvent.toString());
+        System.out.println(orderEvent);
+        KafkaProducerRecord<String, String> record = KafkaProducerRecord.create(
+                TOPIC,
+                orderEvent.itemId,
+                jsonb.toJson(orderEvent));
+        System.out.println(record);
+        producer.send(record, res ->{
+            if (res.failed()) {
+                throw new RuntimeException(res.cause());
+            }
+        });
     }
+
+    @PostConstruct
+    public void postConstruct() {
+        // Config values can be moved to application.properties
+        Map<String, String> config = new HashMap<>();
+        config.put("bootstrap.servers", "localhost:9092");
+        config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("acks", "1");
+        producer = KafkaProducer.create(vertx, config);
+    }
+
 
 }
--- support/README.md
+++ support/README.md
@@ -20,4 +20,40 @@
     "id": "d141c73c-ac62-4534-b5db-7c989040feca"
 }
 ```
+```json
+{
+    "item": "CAKEPOP",
+    "itemId": "c3d154ba-13e8-4e3c-acee-c63df90f34aa",
+    "name": "Goofy",
+    "orderId": "1e435117-1649-42f9-9a5f-2fe71854e56c"
+}
+```
+```json
+{
+    "beverages": [
+        {
+            "item": "COFFEE_WITH_ROOM",
+            "name": "Mickey"
+        },
+        {
+            "item": "COFFEE_BLACK",
+            "name": "Minnie"
+        }
+    ],
+    "kitchenOrders": [
+        {
+            "item": "CAKEPOP",
+            "name": "Mickey"
+        },
+        {
+            "item": "CROISSANT",
+            "name": "Minnie"
+        }
+    ]
+}
+```
 
+Kitchen Order Only
+```json
+{"eventType":"KITCHEN_ORDER_IN","item":"MUFFIN","itemId":"4e3e194f-961a-4a02-923b-26704cf30097","name":"Laurel","orderId":"6593f77c-8d36-4570-8b27-a0bccacf0bfb"}
+```
