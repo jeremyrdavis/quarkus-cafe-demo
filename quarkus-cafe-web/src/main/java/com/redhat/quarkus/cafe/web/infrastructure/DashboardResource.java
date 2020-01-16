63b1cb61cfd444e1868bf26fd438c317f26607c2
Jeremy Davis <jeremyrdavis@mac.com>
2020-01-16 16:17:56 -0500

    updated pom

--- quarkus-cafe-web/src/main/java/com/redhat/quarkus/cafe/web/infrastructure/DashboardResource.java
+++ quarkus-cafe-web/src/main/java/com/redhat/quarkus/cafe/web/infrastructure/DashboardResource.java
@@ -1,22 +1,14 @@
 package com.redhat.quarkus.cafe.web.infrastructure;
 
-import com.redhat.quarkus.cafe.web.domain.CreateOrderCommand;
-import com.redhat.quarkus.cafe.web.domain.DashboardUpdate;
 import io.smallrye.reactive.messaging.annotations.Channel;
-import io.smallrye.reactive.messaging.annotations.Emitter;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.resteasy.annotations.SseElementType;
 import org.reactivestreams.Publisher;
 
 import javax.inject.Inject;
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @Path("/dashboard")
 public class DashboardResource {
