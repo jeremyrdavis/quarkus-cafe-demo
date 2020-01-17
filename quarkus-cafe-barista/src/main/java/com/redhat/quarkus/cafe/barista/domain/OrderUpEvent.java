9050e81e8cb6d524135d7729b4c41c01db1b195e
Jeremy Davis <jeremyrdavis@mac.com>
2020-01-17 06:22:16 -0500

    fix-barista-0117

--- quarkus-cafe-barista/src/main/java/com/redhat/quarkus/cafe/barista/domain/OrderUpEvent.java
+++ quarkus-cafe-barista/src/main/java/com/redhat/quarkus/cafe/barista/domain/OrderUpEvent.java
@@ -5,4 +5,7 @@ public class OrderUpEvent extends BeverageOrder{
     public OrderUpEvent(String orderId, String itemId, String name, Item item) {
         super(EventType.BEVERAGE_ORDER_UP, orderId, itemId, name, item);
     }
+
+    public OrderUpEvent() {
+    }
 }
