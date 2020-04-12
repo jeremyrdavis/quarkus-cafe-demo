1166a0e95d0dd45a73c3e46d619ac894b2ccde87
Jeremy Davis <jeremyrdavis@mac.com>
2020-04-11 22:28:36 -0400

    modified repository

--- quarkus-cafe-core/src/main/java/com/redhat/quarkus/cafe/infrastructure/OrderRepository.java
+++ quarkus-cafe-core/src/main/java/com/redhat/quarkus/cafe/infrastructure/OrderRepository.java
@@ -2,9 +2,15 @@ package com.redhat.quarkus.cafe.infrastructure;
 
 import com.redhat.quarkus.cafe.domain.Order;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import org.bson.types.ObjectId;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class OrderRepository implements PanacheMongoRepository<Order> {
+
+    @Override
+    public void persist(Order order) {
+        order.id = ObjectId.get();
+    }
 }
