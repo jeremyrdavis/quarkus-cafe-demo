fdefca48b86618ce0f726090a46cc18510d45e9f
Jeremy Davis <jeremyrdavis@mac.com>
2020-05-14 21:50:05 -0400

    passing BaristaIT

--- quarkus-cafe-barista/src/test/java/com/redhat/quarkus/cafe/barista/infrastructure/BaristaIT.java
+++ quarkus-cafe-barista/src/test/java/com/redhat/quarkus/cafe/barista/infrastructure/BaristaIT.java
@@ -1,22 +1,64 @@
 package com.redhat.quarkus.cafe.barista.infrastructure;
 
+import com.redhat.quarkus.cafe.barista.domain.EventType;
+import com.redhat.quarkus.cafe.barista.domain.Item;
+import com.redhat.quarkus.cafe.barista.domain.OrderIn;
+import com.redhat.quarkus.cafe.barista.domain.OrderUp;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.Random;
+import java.util.UUID;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest @QuarkusTestResource(KafkaTestResource.class)
 public class BaristaIT {
+
+    Jsonb jsonb = JsonbBuilder.create();
 
     static KafkaConsumer<String, String> kafkaConsumer;
 
     static KafkaProducer<String, String> kafkaProducer;
+
+    @Test
+    public void testOrderIn() {
+        OrderIn orderIn = new OrderIn(UUID.randomUUID().toString(), UUID.randomUUID().toString(), "Lemmy", Item.COFFEE_BLACK);
+        kafkaProducer.send(new ProducerRecord<>("orders", jsonb.toJson(orderIn)));
+
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            assertNull(e);
+        }
+
+        ConsumerRecords<String, String> newRecords = kafkaConsumer.poll(Duration.ofMillis(10000));
+        for (ConsumerRecord<String, String> record : newRecords) {
+            System.out.println(record.value());
+            OrderUp orderUp = jsonb.fromJson(record.value(), OrderUp.class);
+            assertEquals(EventType.BEVERAGE_ORDER_UP, orderUp.eventType);
+            assertEquals("Lemmy", orderUp.name);
+            assertEquals(Item.COFFEE_BLACK, orderUp.item);
+        }
+    }
+
 
     @BeforeAll
     public static void setUp() {
