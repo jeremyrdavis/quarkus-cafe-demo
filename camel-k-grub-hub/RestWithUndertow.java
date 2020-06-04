589df48357ce97ce3e10d8cd51bf75d62d3b90a5
mary cochran <marycochran@marys-MacBook-Pro.local>
2020-06-04 12:45:52 -0400

    started a grubhub format

--- camel-k-grub-hub/RestWithUndertow.java
+++ camel-k-grub-hub/RestWithUndertow.java
@@ -1,4 +1,5 @@
 import org.apache.camel.Exchange;
+import org.apache.camel.model.rest.RestBindingMode;
 
 public class RestWithUndertow extends org.apache.camel.builder.RouteBuilder {
     @Override
@@ -6,15 +7,57 @@ public class RestWithUndertow extends org.apache.camel.builder.RouteBuilder {
         restConfiguration()
             .component("undertow")
             .host("0.0.0.0")
-            .port("8080");
+            .port("8080")
+            .bindingMode(RestBindingMode.auto);
 
         rest()
             .get("/hello")
-            .to("direct:hello");
+            .to("direct:hello")
+            .post("/order").type(GrubHubOrder.class).consumes("application/json")
+            .to("direct:order");
 
         from("direct:hello")
             .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
-            .transform().simple("Hello World!");
+            .transform().simple("Hello!");
+
+        from("direct:order")
+            .log("Body is ${body}");
+
+}
+
+static class GrubHubOrder {
+	private String orderId;
+	private String orderItem;
+    private String name;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderItem() {
+        return orderItem;
+    }
+
+    public void setOrderItem(String orderItem) {
+        this.orderItem = orderItem;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "GrubHubOrder [name=" + name + ", orderId=" + orderId + ", orderItem=" + orderItem + "]";
+    }
 
 }
 }
