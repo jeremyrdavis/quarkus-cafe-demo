2d0d29c9861a40c5a1712562615c3c4cf4a877ba
Jeremy Davis <jeremyrdavis@mac.com>
2020-07-14 15:32:00 -0400

    added new tests

---
+++ quarkus-cafe-core/src/test/java/com/redhat/quarkus/cafe/infrastructure/KafkaServiceBaristaOrderOnlyIT.java
@@ -0,0 +1,65 @@
+package com.redhat.quarkus.cafe.infrastructure;
+
+import com.redhat.quarkus.cafe.domain.*;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.time.Duration;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@QuarkusTest
+@QuarkusTestResource(CafeITResource.class)
+public class KafkaServiceBaristaOrderOnlyIT extends KafkaIT{
+    
+    @InjectMock
+    OrderRepository orderRepository;
+
+    @BeforeEach
+    public void setup() {
+        Mockito.doAnswer(new TestUtil.AssignIdToEntityAnswer(1L)).when(orderRepository).persist(any(Order.class));
+    }
+
+    @Test
+    public void testOrderInBeveragesOnly() throws InterruptedException {
+
+        final List<LineItem> beverages = new ArrayList<>();
+        beverages.add(new LineItem(Item.COFFEE_WITH_ROOM, "Kirk"));
+        beverages.add(new LineItem(Item.ESPRESSO_DOUBLE, "Spock"));
+        final CreateOrderCommand createOrderCommand = new CreateOrderCommand(beverages, null);
+
+        // send the order to Kafka and wait
+        producerMap.get("web-in").send(new ProducerRecord("web-in", jsonb.toJson(createOrderCommand)));
+        Thread.sleep(1000);
+
+        // intercept the messages from the appropriate consumer
+        final KafkaConsumer baristaConsumer = consumerMap.get("barista-in");
+        baristaConsumer.seekToBeginning(new ArrayList<TopicPartition>()); //
+        final ConsumerRecords<String, String> newRecords = baristaConsumer.poll(Duration.ofMillis(2000));
+
+        // verify that the records are of the correct type
+        newRecords.forEach(record -> {
+            System.out.println("baristaOrder: " + record.value());
+            final OrderInEvent orderInEvent = JsonUtil.jsonb.fromJson(record.value(), OrderInEvent.class);
+//            assertBeverageInEvent(orderInEvent);
+            assertTrue(orderInEvent.item.equals(Item.ESPRESSO_DOUBLE) || orderInEvent.item.equals(Item.COFFEE_WITH_ROOM),
+                    "The item should be either a " + Item.ESPRESSO_DOUBLE + " or a " + Item.COFFEE_WITH_ROOM + " not a " + orderInEvent.item);
+        });
+
+        // verify the number of new records
+        //assertEquals(2, newRecords.count());
+    }
+}
