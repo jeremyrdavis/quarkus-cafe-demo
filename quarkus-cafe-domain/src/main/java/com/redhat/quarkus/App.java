e221764fe10a178c9088ef0e81276d8749805e31
Jeremy Davis <jeremyrdavis@mac.com>
2020-07-20 21:11:29 -0400

    compiling

---
+++ quarkus-cafe-domain/src/main/java/com/redhat/quarkus/App.java
@@ -0,0 +1,13 @@
+package com.redhat.quarkus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
