b103e410ae4e6aab9f18728ce3b3470493a40f20
Jeremy Davis <jeremyrdavis@mac.com>
2020-09-16 10:29:12 -0400

    merged

--- support/create-topics.sh
+++ support/create-topics.sh
@@ -3,6 +3,7 @@ kafka-topics --bootstrap-server localhost:9092 --create --partitions 4 --replica
 kafka-topics --bootstrap-server localhost:9092 --create --partitions 4 --replication-factor 1 --topic orders
 kafka-topics --bootstrap-server localhost:9092 --create --partitions 4 --replication-factor 1 --topic barista-in
 kafka-topics --bootstrap-server localhost:9092 --create --partitions 4 --replication-factor 1 --topic kitchen-in
+kafka-topics --bootstrap-server localhost:9092 --create --partitions 4 --replication-factor 1 --topic inventory
 kafka-topics --bootstrap-server localhost:9092 --create --partitions 4 --replication-factor 1 --topic web-in
 kafka-topics --bootstrap-server localhost:9092 --create --partitions 4 --replication-factor 1 --topic web-updates
 kafka-topics --bootstrap-server localhost:9092 --list
